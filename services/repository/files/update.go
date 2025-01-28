f56b10ca8cab59ef2686b12bcf4b865f8fdaff92
davrot <davrot@noreply.codeberg.org>
2025-01-28 17:34:21 +0000

    services/repository/files/update.go aktualisiert

--- services/repository/files/update.go
+++ services/repository/files/update.go
@@ -118,7 +118,7 @@ func ChangeRepoFiles(ctx context.Context, repo *repo_model.Repository, doer *use
 			
 			for _, entry := range allEntries {
 				filename := entry.Name()
-				
+				// Here we need to remove directories... I don't know why...
 				if len(filename) > 0 {
 					newOptsFiles = append(newOptsFiles, &ChangeRepoFile{
 						Operation: "delete",
